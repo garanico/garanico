df520d3ece2c615460a6298d469abb154f1c61e2
Grace Aranico <grace.aranico@gmail.com>
2022-04-24 11:06:13 -0700

    feat(footer): Add Footer and footer CSS

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Footer() {
+  return (
+    <div id="footer">
+        <p>Made with  &#10024; by Grace Aranico</p>
+    </div>
+  )
+}
+
+export default Footer
