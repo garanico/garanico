1826d232ca59706c7d30c0ceee09e77c4d662a53
Grace Aranico <grace.aranico@gmail.com>
2022-04-24 22:33:59 -0700

    feat(contact links): Add button and social media links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './Components/Footer';
 import Nav from './Components/Nav';
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Homepage />
       <Footer />
       
     </div>
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,8 +1,57 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import grace from '../img/grace.JPG';
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+    <main id="homepage">
+      <section className="container-1">
+
+      <div className="intro-photo">
+          <img className="photo" src={grace} alt="Grace Aranico's headshot photo" />
+        </div>
+
+        <div className="intro-text">
+          <p>Oh hey!</p>
+          <h1 className="font-playfair">I'm Grace</h1>
+          <p>A Front-End Web Developer passionate about creating digital experiences that are equal parts functional and beautiful.</p>
+
+
+          <div className="contact-links">
+          <div className="button">
+            <button>
+              <p>view my resume</p>
+            </button>
+          </div>
+
+          <div className="social-icons">
+            <a href="https://github.com/garanico" target="_blank">
+              <i className="fab fa-github-square"></i>
+            </a>
+
+            <a href="https://linkedin.com/in/gracearanico/" target="_blank">
+              <i className="fab fa-linkedin"></i>
+            </a>
+
+            <a href="https://instagram.com/gracieissoamzin" target="_blank">
+              <i className="fab fa-instagram"></i>
+            </a>  
+          </div>
+          </div>
+
+        </div>
+
+
+
+      </section>
+
+      <div>
+
+
+      </div>
+    </main>
+    </>
   )
 }
 
