4c24ee912588466bd8f3abb98c3faa380b448e72
Grace Aranico <grace.aranico@gmail.com>
2022-04-28 15:05:56 -0700

    feat(project main): Add projects to page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Footer from './Components/Footer';
 import Nav from './Components/Nav';
 import Homepage from './Components/Homepage';
 import About from './Components/About';
+import ProjectMain from './Components/Project-Pages/ProjectMain';
 
 function App() {
   return (
     <div className="App">
       {/* <Nav /> */}
-      <About />
+      <ProjectMain />
       <Footer />
       
     </div>
--- src/Components/Project-Pages/ProjectMain.js
+++ src/Components/Project-Pages/ProjectMain.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import '../../../src/App.css';
+import decidr from '../../img/project-img/decidr.png';
+import hauntedHouse from '../../img/project-img/haunted-house.png'
+import magic8Ball from '../../img/project-img/magic8-ball.png'
+import triviaChill from '../../img/project-img/trivia-chill.png'
+import w4b from '../../img/project-img/w4b.png'
 
 function ProjectMain() {
   return (
-    <div>ProjectMain</div>
+    <main id="projects">
+      <div className="project-heading">
+        <p>take a look at my</p>
+        <h1 className="font-playfair">projects</h1>
+      </div>
+
+      <div className="card-container">
+        <div className="card-subtitle">
+          <p>click on any project to learn more!</p>
+        </div>
+        
+        <a href="https://garanico.github.io/decidr-app/" rel="noopener">
+          <img className="card" src={decidr} alt="decidr decision making app" />
+        </a>
+
+        <a href="https://luceroweb.github.io/haunted-house-game/#/" rel="noopener">
+          <img className="card" src={hauntedHouse} alt="The Haunted House Game choose your own adventure game" />
+        </a>
+
+        <a href="https://garanico.github.io/javascript-final-project/" rel="noopener">
+          <img className="card" src={magic8Ball} alt="Harry Potter themed magic 8 ball project" />
+        </a>
+
+        <a href="https://garanico.github.io/decidr-app/" rel="noopener">
+          <img className="card" src={triviaChill} alt="An art-deco designed movie trivia game" />
+        </a>
+
+        <a href="https://garanico.github.io/w4b-final-project/" rel="noopener">
+          <img className="card" src={w4b} alt="A Harry Potter Maurader's Map themed website" />
+        </a>
+
+        <a href="https://garanico.github.io/decidr-app/" rel="noopener">
+          <img className="card" src={decidr} alt="decidr decision making app" />
+        </a>
+
+
+
+      </div>
+
+
+
+    </main>
   )
 }
 
